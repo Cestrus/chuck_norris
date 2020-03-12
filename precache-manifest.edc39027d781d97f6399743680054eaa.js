256a19c112894dc83d6cc08bc316ef8e5af9acef
Alexey Savchenko <wasxela@gmail.com>
2020-03-12 20:21:33 +0200

    Updates

---
+++ precache-manifest.edc39027d781d97f6399743680054eaa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a6e8245513b6dde418b3923c53d49e3",
+    "url": "/chuck_norris/index.html"
+  },
+  {
+    "revision": "f866fa92ac1c29727bf1",
+    "url": "/chuck_norris/static/css/main.0f873ce8.chunk.css"
+  },
+  {
+    "revision": "52181f0820e62da0e14a",
+    "url": "/chuck_norris/static/js/2.16ae8346.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/chuck_norris/static/js/2.16ae8346.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f866fa92ac1c29727bf1",
+    "url": "/chuck_norris/static/js/main.6282b91d.chunk.js"
+  },
+  {
+    "revision": "bb00040cc2aca7c8de2b",
+    "url": "/chuck_norris/static/js/runtime-main.2dfe87c1.js"
+  },
+  {
+    "revision": "beaa59fd10475f710d2ff182c3e02878",
+    "url": "/chuck_norris/static/media/chuck-norris.beaa59fd.png"
+  }
+]);
